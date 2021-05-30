8ec8177a220f97c16b3cdd4ed1bc488e47c6285e
hrntsm <contact@hrntsm.com>
2021-05-30 10:39:05 +0900

    Add sample to box

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import { Editor } from "./building-editor/index.js";
 const editor = new Editor();
 document.body.appendChild(editor.renderer.domElement);
@@ -11,6 +12,13 @@ const init = () => {
     const height = window.innerHeight;
     editor.renderer.setPixelRatio(window.devicePixelRatio);
     editor.renderer.setSize(width, height);
+
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshBasicMaterial();
+    const box = new THREE.Mesh(geometry, material);
+    editor.addObject(box);
+    editor.select(box);
+
     editor.render();
 };
 
